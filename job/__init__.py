c21d79c099f11103cb87ea800cdcc282d97f9c5c
zongxin.xu <zongxin.xu@goodeep.com>
2025-09-30 11:47:26 +0800

    update gitignore

--- job/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-X平台爬虫定时任务模块
-"""
-
-from .scheduler import XSpiderScheduler
-from .tasks import crawl_followed_users_task
-
-__all__ = ['XSpiderScheduler', 'crawl_followed_users_task']
--- sms/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-SMS通知模块
-支持飞书机器人等多种通知方式
-"""
-
-from .feishu_bot import FeishuBot
-
-__all__ = ['FeishuBot']
